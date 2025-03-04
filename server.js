60683369486ea2633c91f16290daf96683f65bbb
olatundesleek <olatundesleek@gmail.com>
2025-03-04 08:52:56 -0800

    added socket installation

--- server.js
+++ server.js
@@ -2,8 +2,55 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/user.routes");
 const checkJson = require("./middleware/checkjson");
+const http = require('http')
 require("dotenv").config();
 const app = express();
+const cors = require('cors')
+const { Server } = require('socket.io');
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow your frontend to connect
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"], // Optional: If you're sending custom headers
+    credentials: true // Optional: If you're using cookies or other credentials
+  }
+})
+const onlineUsers = new Map()
+
+io.on('connection', (socket) => {
+  console.log(socket.id + ' connected');
+
+  
+
+  // Emit a message when the client connects
+  io.emit('server message', socket.id + ' connected');
+
+  socket.on('register',(username)=>{
+    onlineUsers.set(username,socket.id)
+  })
+  //Listen for new chat messages
+  socket.on('chatMessage', (msg) => {
+    console.log('message received: ' + msg);
+    
+    // Emit the message to all connected clients (including the sender)
+    io.emit('server message',socket.id + ":  "+msg);
+  });
+
+  // Handle disconnection event
+  socket.on('disconnect', () => {
+    console.log(socket.id + ' disconnected');
+    socket.emit('server message', socket.id + ' disconnected');
+  });
+});
+
+
+
+
+
+
 
 const MailDev = require("maildev");
 
@@ -21,7 +68,7 @@ maildev.listen(() => {
 // maildev.on("new", (email) => {
 //   console.log("New email received:", email.subject);
 // });
-
+app.use(cors())
 app.use(express.json());
 app.use(checkJson);
 
@@ -34,4 +81,4 @@ app.use("/api", userRouter);
 let port = process.env.PORT;
 console.log(port);
 
-app.listen(port);
+server.listen(port);
